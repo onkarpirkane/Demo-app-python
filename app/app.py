217dc0c66a5ede560555cf4c1c3fc50ab40a6b96
onkarpirkane <ompirkane@gmail.com>
2026-04-01 17:38:29 +0530

    V2

--- app/app.py
+++ app/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter(
+    'demo_requests_total',
+    'Total App Requests'
+)
+
 @app.route("/")
 def home():
-    return "Hello from Kubernetes Helm App 🚀"
+    REQUEST_COUNT.inc()
+    return "App is running"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(),
+                    mimetype="text/plain")
+
+app.run(host="0.0.0.0", port=5000)
